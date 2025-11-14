2d81361a13845a5eec09b38b609513846f681038
boscot553 <yizhengtian2011@gmail.com>
2025-11-14 10:16:27 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,36 @@
+import tkinter as tk
+import keyboard
+
+x = 0
+
+root = tk.Tk()
+root.title("Cookie clicker")
+root.geometry("300x200")
+label = tk.Label(root, text="0")
+label.pack(pady=20)
 
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class cookies():
+    def __init__ (self, num):
+        self.num = num
+    
+def on_button_click():
+    cookie.num += 1    
+    label.config(text=cookie.num)
+        
+cookie = cookies(0)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def upgrade():
+    cost1 = 9
+    if cookie.num > cost1:
+        cookie.num -= 10
+        label.config(text=cookie.num)
+        cost1 += 1
+        cost1 = cost1 * 2
+
+keyboard.add_hotkey('z', upgrade)
+button = tk.Button(root, text="Click Me", command=on_button_click, width = 20, height= 20)
+button.pack()
+
+root.mainloop()
