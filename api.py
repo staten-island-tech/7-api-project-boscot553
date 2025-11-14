4516fa565dbe66dca45ec4a68e27d5218dba97d8
boscot553 <yizhengtian2011@gmail.com>
2025-11-14 09:32:23 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,2 +1,10 @@
-import _tkinter
-from _tkinter import Tcl_Obj
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
