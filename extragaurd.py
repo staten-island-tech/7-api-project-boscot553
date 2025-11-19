03a25d3d682493e097063e3f237386483a9f0d90
boscot553 <yizhengtian2011@gmail.com>
2025-11-19 10:14:37 -0500

    p

--- extragaurd.py
+++ extragaurd.py
@@ -1,7 +1,9 @@
 def isvalid(email, password):
     ats = 0
     ints = 0
+    caps = 0
     integer =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    captials =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     if isinstance(email, str):
         ats = 0
     else:
@@ -24,10 +26,13 @@ def isvalid(email, password):
     for char in password:
         if char in integer:
             ints += 1
-        if char.isupper():
-            print("Password must contain at least one upper case letter")
-            return
+        if char in captials:
+            caps += 1
     if ints < 1:
         print("Password must have at least one integer")
         return
-isvalid("jjjjj@", "gGgggggggg")
+    if caps < 1:
+        print("Password must have at least one captial letter")
+        return
+    print(f"Email: {email}, Password: {password}")
+isvalid("J@.net", "2025Jiscool")
---
+++ python.py
@@ -0,0 +1,3 @@
+x = "2"
+if x.isdigit():
+    print(x)
