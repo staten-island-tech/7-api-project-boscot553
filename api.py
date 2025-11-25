4059f7bdc03fbc85d5ee79f76a859e93b62c3d3d
boscot553 <yizhengtian2011@gmail.com>
2025-11-25 09:29:02 -0500

    p

--- api.py
+++ api.py
@@ -1,9 +1,7 @@
 import tkinter as tk
-import keyboard
 
 x = 0
-
-
+cps = 1000000
 root = tk.Tk()
 root.title("Cookie clicker")
 root.geometry("3000x1250")
@@ -11,6 +9,11 @@ label = tk.Label(root, text="0")
 label.pack(pady=20)
 label.place(x=885, y=150)
 label.config(font=("Verdana", 30))
+
+labelup = tk.Label(root, text="10")
+labelup.pack(pady=20)
+labelup.place(x=150, y=925)
+labelup.config(font=("Verdana", 15))
 
 class cookies():
     def __init__ (self, num, cost, production):
@@ -20,7 +23,7 @@ class cookies():
 
 
 def on_button_click():
-    cookie.num += cookie.production    
+    cookie.num += cookie.production
     label.config(text=cookie.num)
         
 cookie = cookies(0, 10, 1)
@@ -30,8 +33,8 @@ def upgrade():
         cookie.num -= cookie.cost
         label.config(text=cookie.num)
         cookie.cost = cookie.cost * 2
-        cookie.production += 1
-        
+        cookie.production += 0.0000001 * cps
+        labelup.config(text=cookie.cost)
 button = tk.Button(root, text="Click Me", command=on_button_click, width = 20, height= 10)
 upgrade_button = tk.Button(root, text="Upgrade", command=upgrade, width = 20, height= 5)
 button.pack()
